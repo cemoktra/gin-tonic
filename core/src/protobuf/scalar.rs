3bb12e0b6b5640e8950042e0e883e001656dec79
Bastian Schubert <bastian.schubert@dock.financial>
2024-06-13 14:26:49 +0200

    nested

--- core/src/protobuf/scalar.rs
+++ core/src/protobuf/scalar.rs
@@ -1,5 +1,5 @@
 use crate::protobuf::wire::{WireType, WireTypeView};
-use crate::protobuf::{Error, FromWire, IntoWire};
+use crate::protobuf::{Error, FromWire, IntoWire, ProtocolBuffer};
 use integer_encoding::VarInt;
 
 impl FromWire for u32 {
@@ -33,6 +33,37 @@ impl IntoWire for u32 {
     }
 }
 
+impl FromWire for i32 {
+    fn from_wire(wire: WireTypeView) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        match wire {
+            WireTypeView::VarInt(data) => {
+                let (value, _) = i32::decode_var(data).ok_or(Error::InvalidVarInt)?;
+                Ok(value)
+            }
+            WireTypeView::FixedI32(data) => {
+                let array: [u8; 4] = data.try_into().expect("I32 is always 4 bytes");
+                Ok(i32::from_be_bytes(array))
+            }
+            _ => Err(Error::UnexpectedWireType),
+        }
+    }
+}
+
+impl IntoWire for i32 {
+    fn into_wire(self) -> WireType {
+        let mut data = [0u8; 10];
+        let size = self.encode_var(&mut data);
+        WireType::VarInt(data, size)
+    }
+
+    fn size_hint(&self, tag: u32) -> usize {
+        self.required_space() + tag.required_space()
+    }
+}
+
 impl FromWire for String {
     fn from_wire(wire: WireTypeView) -> Result<Self, Error>
     where
@@ -53,5 +84,37 @@ impl IntoWire for String {
     fn size_hint(&self, tag: u32) -> usize {
         let len = self.len();
         len.required_space() + tag.required_space() + len
+    }
+}
+
+impl<T> IntoWire for T
+where
+    T: ProtocolBuffer,
+{
+    fn into_wire(self) -> WireType {
+        println!("message requires {} bytes", self.size_hint());
+        let mut buffer = Vec::with_capacity(self.size_hint());
+        self.serialize(&mut buffer).expect("must work");
+        WireType::LengthEncoded(buffer)
+    }
+
+    fn size_hint(&self, tag: u32) -> usize {
+        let size = self.size_hint();
+        tag.required_space() + size.required_space() + size
+    }
+}
+
+impl<T> FromWire for T
+where
+    T: ProtocolBuffer,
+{
+    fn from_wire(wire: WireTypeView) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        match wire {
+            WireTypeView::LengthEncoded(data) => T::deserialize(data),
+            _ => Err(Error::UnexpectedWireType),
+        }
     }
 }
