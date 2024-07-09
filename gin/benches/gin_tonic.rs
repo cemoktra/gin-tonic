12c343f97a3e8c2686d34ab74aff6e9ae2f0cf12
Bastian Schubert <cemoktra@.github.com>
2024-07-09 15:44:08 +0200

    extend benchmarks

--- gin/benches/gin_tonic.rs
+++ gin/benches/gin_tonic.rs
@@ -5,6 +5,8 @@ fn main() {
 #[cfg(feature = "uuid_bytes")]
 mod gin_bench {
 
+    use std::{collections::HashMap, path::PathBuf};
+
     use gin_tonic::Message;
 
     /// this would normally be generated
@@ -12,38 +14,88 @@ mod gin_bench {
     pub struct GinTonic {
         #[gin(tag = 1u32)]
         pub uuid: uuid::Uuid,
-        #[gin(tag = 2u32)]
-        pub ip: std::net::Ipv4Addr,
+        #[gin(tag = 2u32, cardinality = "repeated")]
+        pub ip: Vec<std::net::Ipv4Addr>,
+        #[gin(tag = 3u32)]
+        pub text: String,
+        #[gin(tag = 4u32, kind = "message")]
+        pub nested: GinTonicNested,
     }
 
-    #[divan::bench]
+    #[derive(Clone, Debug, Message)]
+    pub struct GinTonicNested {
+        #[gin(tag = 1u32, kind = "map")]
+        pub counts: HashMap<PathBuf, u64>,
+    }
+
+    #[divan::bench(min_time = std::time::Duration::from_secs(1))]
     fn ser(bencher: divan::Bencher) {
         use gin_tonic::gin_tonic_core::Message;
 
-        let data = GinTonic {
+        let mut counts = HashMap::new();
+        counts.insert("a".into(), 1);
+        counts.insert("b".into(), 2);
+        counts.insert("c".into(), 3);
+        counts.insert("d".into(), 4);
+        counts.insert("e".into(), 5);
+
+        let data = divan::black_box(GinTonic {
             uuid: uuid::Uuid::new_v4(),
-            ip: std::net::Ipv4Addr::LOCALHOST,
-        };
+            ip: vec![
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+            ],
+            text: "Benchmarking some longer text for fucks sake".into(),
+            nested: GinTonicNested { counts },
+        });
 
         let size = data.size_hint();
-        let mut buffer = bytes::BytesMut::with_capacity(size);
-        let buffer_ref = &mut buffer;
+        let mut buffer = divan::black_box(bytes::BytesMut::with_capacity(size));
+        let buffer_ref = divan::black_box(&mut buffer);
 
         bencher.bench_local(move || data.clone().serialize(buffer_ref));
     }
 
-    #[divan::bench]
+    #[divan::bench(min_time = std::time::Duration::from_secs(1))]
     fn de(bencher: divan::Bencher) {
         use gin_tonic::gin_tonic_core::Message;
 
-        let data = GinTonic {
+        let mut counts = HashMap::new();
+        counts.insert("a".into(), 1);
+        counts.insert("b".into(), 2);
+        counts.insert("c".into(), 3);
+        counts.insert("d".into(), 4);
+        counts.insert("e".into(), 5);
+
+        let data = divan::black_box(GinTonic {
             uuid: uuid::Uuid::new_v4(),
-            ip: std::net::Ipv4Addr::LOCALHOST,
-        };
+            ip: vec![
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+                std::net::Ipv4Addr::LOCALHOST,
+                std::net::Ipv4Addr::BROADCAST,
+            ],
+            text: "Benchmarking some longer text for fucks sake".into(),
+            nested: GinTonicNested { counts },
+        });
 
         let size = data.size_hint();
-        let mut buffer = bytes::BytesMut::with_capacity(size);
-        let buffer_ref = &mut buffer;
+        let mut buffer = divan::black_box(bytes::BytesMut::with_capacity(size));
+        let buffer_ref = divan::black_box(&mut buffer);
         data.clone().serialize(buffer_ref);
 
         bencher.bench_local(move || {
--- gin/benches/prost.rs
+++ gin/benches/prost.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, path::PathBuf};
+
 use prost::Message;
 
 /// this would normally be generated
@@ -5,14 +7,31 @@ use prost::Message;
 pub struct Prost {
     #[prost(bytes = "vec", tag = "1")]
     pub uuid: ::prost::alloc::vec::Vec<u8>,
-    #[prost(uint32, tag = "2")]
-    pub ip: u32,
+    #[prost(uint32, tag = "2", repeated)]
+    pub ip: Vec<u32>,
+    #[prost(string, tag = "3")]
+    pub text: String,
+    #[prost(message, tag = "4")]
+    pub nested: Option<ProstNested>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ProstNested {
+    #[prost(map = "string, uint64", tag = "1")]
+    pub counts: HashMap<String, u64>,
 }
 
 #[derive(Clone)]
 pub struct Rust {
     pub uuid: uuid::Uuid,
-    pub ip: std::net::Ipv4Addr,
+    pub ip: Vec<std::net::Ipv4Addr>,
+    pub text: String,
+    pub nested: RustNested,
+}
+
+#[derive(Clone)]
+pub struct RustNested {
+    pub counts: HashMap<PathBuf, u64>,
 }
 
 impl From<Prost> for Rust {
@@ -20,7 +39,9 @@ impl From<Prost> for Rust {
         let uuid_array: [u8; 16] = value.uuid.try_into().expect("benchmark works");
         Self {
             uuid: uuid::Uuid::from_bytes(uuid_array),
-            ip: value.ip.into(),
+            ip: value.ip.into_iter().map(Into::into).collect(),
+            text: value.text,
+            nested: value.nested.unwrap().into(),
         }
     }
 }
@@ -29,7 +50,33 @@ impl From<Rust> for Prost {
     fn from(value: Rust) -> Self {
         Self {
             uuid: value.uuid.as_bytes().to_vec(),
-            ip: value.ip.into(),
+            ip: value.ip.into_iter().map(Into::into).collect(),
+            text: value.text,
+            nested: Some(value.nested.into()),
+        }
+    }
+}
+
+impl From<ProstNested> for RustNested {
+    fn from(value: ProstNested) -> Self {
+        Self {
+            counts: value
+                .counts
+                .into_iter()
+                .map(|(k, v)| (k.into(), v))
+                .collect(),
+        }
+    }
+}
+
+impl From<RustNested> for ProstNested {
+    fn from(value: RustNested) -> Self {
+        Self {
+            counts: value
+                .counts
+                .into_iter()
+                .map(|(k, v)| (k.display().to_string(), v))
+                .collect(),
         }
     }
 }
@@ -38,18 +85,38 @@ fn main() {
     divan::main();
 }
 
-#[divan::bench]
+#[divan::bench(min_time = std::time::Duration::from_secs(1))]
 fn ser(bencher: divan::Bencher) {
-    let data = Rust {
+    let mut counts = HashMap::new();
+    counts.insert("a".into(), 1);
+    counts.insert("b".into(), 2);
+    counts.insert("c".into(), 3);
+    counts.insert("d".into(), 4);
+    counts.insert("e".into(), 5);
+
+    let data = divan::black_box(Rust {
         uuid: uuid::Uuid::new_v4(),
-        ip: std::net::Ipv4Addr::LOCALHOST,
-    };
+        ip: vec![
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+        ],
+        text: "Benchmarking some longer text for fucks sake".into(),
+        nested: RustNested { counts },
+    });
 
     let prost_data: Prost = data.clone().into();
 
     let size = prost_data.encoded_len();
-    let mut buffer = Vec::with_capacity(size);
-    let buffer_ref = &mut buffer;
+    let mut buffer = divan::black_box(Vec::with_capacity(size));
+    let buffer_ref = divan::black_box(&mut buffer);
 
     bencher.bench_local(move || {
         let prost_data: Prost = data.clone().into();
@@ -57,20 +124,40 @@ fn ser(bencher: divan::Bencher) {
     });
 }
 
-#[divan::bench]
+#[divan::bench(min_time = std::time::Duration::from_secs(1))]
 fn de(bencher: divan::Bencher) {
-    let data = Rust {
+    let mut counts = HashMap::new();
+    counts.insert("a".into(), 1);
+    counts.insert("b".into(), 2);
+    counts.insert("c".into(), 3);
+    counts.insert("d".into(), 4);
+    counts.insert("e".into(), 5);
+
+    let data = divan::black_box(Rust {
         uuid: uuid::Uuid::new_v4(),
-        ip: std::net::Ipv4Addr::LOCALHOST,
-    };
+        ip: vec![
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+            std::net::Ipv4Addr::LOCALHOST,
+            std::net::Ipv4Addr::BROADCAST,
+        ],
+        text: "Benchmarking some longer text for fucks sake".into(),
+        nested: RustNested { counts },
+    });
 
     let prost_data: Prost = data.clone().into();
 
     let size = prost_data.encoded_len();
 
-    let mut buffer = bytes::BytesMut::with_capacity(size);
+    let mut buffer = divan::black_box(bytes::BytesMut::with_capacity(size));
     prost_data.encode(&mut buffer).expect("benchmark works");
-    let buffer = buffer.freeze();
+    let buffer = divan::black_box(buffer.freeze());
 
     bencher.bench_local(move || {
         let prost_data = Prost::decode(&*buffer).expect("benchmark works");
