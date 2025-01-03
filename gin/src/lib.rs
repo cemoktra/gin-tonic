8c5f2dc758445854e3da6efb7a33b80acb233f60
Jezza <jeremy.a.barrow@gmail.com>
2025-01-03 17:32:05 +0100

    Split out functionality into more features.

--- gin/src/lib.rs
+++ gin/src/lib.rs
@@ -1,16 +1,24 @@
 //! gin-tonic wrapepr for code generation (models and service)
 #[cfg(feature = "tonic")]
 mod codec;
+
+#[cfg(feature = "generator")]
 mod codegen;
 
 #[cfg(feature = "tonic")]
 pub use codec::GinCodec;
+
+#[cfg(feature = "generator")]
 pub use codegen::CompileConfig;
 
 pub use gin_tonic_core;
+
+#[cfg(feature = "derive")]
 pub use gin_tonic_derive;
 
 pub use gin_tonic_core::{types, types::PbOneOf, types::PbType, Decode, DecodeError, Encode, Tag};
+
+#[cfg(feature = "derive")]
 pub use gin_tonic_derive::{Enumeration, Message, OneOf};
 
 // Re-export the alloc crate for use within derived code.
